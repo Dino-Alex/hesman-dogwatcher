f1549a0bfd90b5e4b877dee3cf9a8d483f381191
Dino-Alex <dolly30209@gmail.com>
2022-08-20 18:37:32 +0700

    update build 3

---
+++ src/views/Info/Tokens/Modal/ModalCreate.tsx
@@ -0,0 +1,98 @@
+import { Button, Flex, Input, InputGroup, Text } from '@pancakeswap/uikit'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import { addProductClient, getProductClient } from 'views/Info/components/InfoTables/config'
+import SetPriceStage from 'views/Nft/market/components/BuySellModals/SellModal/SetPriceStage'
+
+interface Proptype {
+  onRefresh?: (newValue) => void
+}
+const ModalCreate: React.FC<Proptype> = ({ onRefresh }) => {
+  const [name, setName] = useState('')
+  const [address, setAddress] = useState('')
+  const [limit, setLimit] = useState('')
+  const [posts, setPosts] = useState([])
+
+  const tokenAuth = localStorage.getItem('token')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const resp = await axios
+        .post(
+          addProductClient,
+          { name: name, address: address, limit: limit },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              token: `${tokenAuth}`,
+            },
+          },
+        )
+        .then((response) => {
+          setPosts([response.data, ...posts])
+        })
+      onRefresh(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <Flex width="100%" flexDirection="column" justifyContent="center" alignItems="center">
+      <Flex width="50%" flexDirection="column" justifyContent="center" alignItems="center">
+        <Flex mt="2rem" width="100%" justifyContent="center" alignItems="center">
+          <Text color="primary">Name: </Text>
+          <CustomInputGroup>
+            <Input
+              type="name"
+              name="name"
+              placeholder="enter an name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              color="primary"
+            />
+          </CustomInputGroup>
+        </Flex>
+        <Flex mt="2rem" width="100%" justifyContent="center" alignItems="center">
+          <Text color="primary">address: </Text>
+          <CustomInputGroup>
+            <Input
+              type="address"
+              name="address"
+              placeholder="enter an address"
+              onChange={(e) => setAddress(e.target.value)}
+              color="primary"
+            />
+          </CustomInputGroup>
+        </Flex>
+        <Flex mt="2rem" width="100%" justifyContent="center" alignItems="center">
+          <Text color="primary">Limit: </Text>
+          <CustomInputGroup>
+            <Input
+              type="limit"
+              name="limit"
+              placeholder="enter an limit"
+              value={limit}
+              onChange={(e) => setLimit(e.target.value)}
+              color="primary"
+            />
+          </CustomInputGroup>
+        </Flex>
+      </Flex>
+      <Flex mt="2rem">
+        <Button type="submit" onClick={handleSubmit}>
+          Create
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default ModalCreate
+
+const CustomInputGroup = styled(InputGroup)`
+  background: 'primary';
+  margin-left: 2rem;
+`
---
+++ src/views/Info/components/InfoTables/config.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const getProductClient = axios.create({
+  baseURL: 'https://dog-watcher-api.deltalabsjsc.com:4001/api/v1/products',
+})
+
+export const addProductClient = 'https://dog-watcher-api.deltalabsjsc.com:4001/api/v1/admin/product/new'
+
+export const deleteProductClient = 'https://dog-watcher-api.deltalabsjsc.com:4001/api/v1/admin/product/'
+
+export const updateProductClient = 'https://dog-watcher-api.deltalabsjsc.com:4001/api/v1/admin/product/'
+
+export const getSingleProductClient = axios.create({
+  baseURL: 'https://dog-watcher-api.deltalabsjsc.com:4001/api/v1/product/',
+})
+
+export const loginClient = axios.create({
+  baseURL: 'https://dog-watcher-api.deltalabsjsc.com:4001/api/v1/login',
+})
