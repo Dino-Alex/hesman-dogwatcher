f171a8e66605341ddb09a66c28686be58205ebfd
Dino-Alex <dolly30209@gmail.com>
2022-08-24 09:41:17 +0700

    change title price

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -1,11 +1,8 @@
 import { useEffect } from 'react'
-import { useCakeBusdPrice } from 'hooks/useBUSDPrice'
 
 const useGetDocumentTitlePrice = () => {
-  const cakePriceBusd = useCakeBusdPrice()
   useEffect(() => {
-    const cakePriceBusdString = cakePriceBusd ? cakePriceBusd.toFixed(2) : ''
-    document.title = `Pancake Swap - ${cakePriceBusdString}`
-  }, [cakePriceBusd])
+    document.title = `Delta Labs`
+  }, [])
 }
 export default useGetDocumentTitlePrice
